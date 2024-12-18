1d2c3ffef21cadf8eef01ceaae77e0e88fe8fc55
Abbas <3bbas.74@gmail.com>
2024-12-18 18:36:45 +0200

    solve Q14

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q13
+            //Console.WriteLine("Enter the word");
+            //string x = Console.ReadLine();
+            //string reversed = "";
+            //for (int i = x.Length - 1; i >= 0; i--)
+            //{
+            //    reversed += x[i];
+            //}
+            //Console.WriteLine($"revesed word is:{reversed} ");
+            #endregion
+            #region Q14
+            Console.WriteLine("Enter the Num");
+            string x = Console.ReadLine();
+            string reversed = "";
+            for (int i = x.Length - 1; i >= 0; i--)
+            {
+                reversed = reversed + x[i];
+            }
+            int convert = int.Parse(reversed);
+            Console.WriteLine($"The reversed number is {convert}");
+            #endregion
+
+
         }
     }
 }
