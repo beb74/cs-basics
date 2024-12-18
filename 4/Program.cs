4bda8fa4ac29a572c9cb7991b8505b0404c7fede
Abbas <3bbas.74@gmail.com>
2024-12-18 18:03:28 +0200

    for loop

--- 4/Program.cs
+++ 4/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-    
+        #region Q13
+        Console.WriteLine("Enter the word");
+        string x = Console.ReadLine();
+        string reversed = "";
+        for (int i = x.Length - 1; i >= 0; i--)
+        {
+            reversed += x[i];
+        }
+        Console.WriteLine($"revesed word is:{reversed} ");
+        #endregion
+
+
     }
 }
