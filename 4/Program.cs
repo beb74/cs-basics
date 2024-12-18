84fd3dab52db8f696cbd5abf1c9b426579ab3d48
Abbas <3bbas.74@gmail.com>
2024-12-18 18:06:06 +0200

    for loop 2

--- 4/Program.cs
+++ 4/Program.cs
@@ -5,14 +5,25 @@ class Program
     static void Main(string[] args)
     {
         #region Q13
-        Console.WriteLine("Enter the word");
+        //Console.WriteLine("Enter the word");
+        //string x = Console.ReadLine();
+        //string reversed = "";
+        //for (int i = x.Length - 1; i >= 0; i--)
+        //{
+        //    reversed += x[i];
+        //}
+        //Console.WriteLine($"revesed word is:{reversed} ");
+        #endregion
+        #region Q14
+        Console.WriteLine("Enter the Num");
         string x = Console.ReadLine();
         string reversed = "";
         for (int i = x.Length - 1; i >= 0; i--)
         {
-            reversed += x[i];
+            reversed = reversed + x[i];
         }
-        Console.WriteLine($"revesed word is:{reversed} ");
+        int convert = int.Parse(reversed);
+        Console.WriteLine($"The reversed number is {convert}");
         #endregion
 
 
